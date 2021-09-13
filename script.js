016542703fda5ad385fe2956c9109746588aa543
AsafeEt <79912213+AsafeEt@users.noreply.github.com>
2021-09-13 12:31:09 +0300

    big changes

--- script.js
+++ script.js
@@ -5,10 +5,14 @@ const footer = document.querySelector(".footer");
 const bigDisplayCard = document.querySelector(".big-country-card");
 const moreCountryContainer = document.querySelector(".more-country-info");
 const overlay = document.querySelector(".overlay");
+const searchInput = document.getElementById("search");
+const searchWrapper = document.querySelector(".wrapper");
+const resultsWrapper = document.querySelector(".results");
+
+input.focus();
 
 searchBtn.addEventListener("click", function () {
   let numCountries = displayContainer.childElementCount;
-  console.log(numCountries);
 
   if (numCountries === 3) {
     displayContainer.firstElementChild.remove();
@@ -18,11 +22,9 @@ searchBtn.addEventListener("click", function () {
 
 displayContainer.addEventListener("click", function (e) {
   let countryClick = e.target;
-  // console.log(countryClick);
-  // console.log(countryClick.dataset.code);
 
   let countryCode = countryClick.dataset.code;
-  console.log(countryCode);
+
   moreCountryData(countryCode);
 });
 
@@ -32,21 +34,18 @@ moreCountryContainer.addEventListener("click", function () {
   footer.classList.remove("hidden");
 });
 
-// closeBigCountryInfo();
-
 const country = async function () {
   try {
     const res = await fetch(
       `https://restcountries.eu/rest/v2/name/${input.value}`
     );
 
+    console.log(res);
     const [countryJson] = await res.json();
 
     if (!res.ok) {
       throw new Error(`HTTP error! status: ${res.status}`);
     }
-
-    console.log(countryJson);
 
     displayContainer.insertAdjacentHTML(
       "beforeend",
@@ -67,6 +66,9 @@ const country = async function () {
     input.value = "";
   } catch (e) {
     console.error(e);
+    alert(`We couldn't find this country, please type again`);
+    input.value = "";
+    input.focus();
   }
 };
 
@@ -102,9 +104,265 @@ const moreCountryData = async function (country) {
     moreCountryContainer.classList.remove("hidden");
     overlay.classList.remove("hidden");
     footer.classList.add("hidden");
-
-    console.log(moreCountryJson);
   } catch (e) {
     console.error(e);
+    input.focus();
   }
 };
+
+// live search------
+
+searchInput.addEventListener("keyup", function () {
+  let results = [];
+  let input = searchInput.value;
+  if (input.length) {
+    results = searchable.filter((item) => {
+      return item.toLowerCase().includes(input.toLowerCase());
+    });
+  }
+  renderResults(results);
+});
+
+const renderResults = function (results) {
+  if (!results.length) {
+    return searchWrapper.classList.remove("show");
+  }
+
+  const content = results
+    .map((item) => {
+      return `<li>${item}</li>`;
+    })
+    .join("");
+  console.log(content);
+
+  searchWrapper.classList.add("show");
+  resultsWrapper.innerHTML = `<ul>${content}</ul>`;
+};
+
+resultsWrapper.addEventListener("click", function (e) {
+  let country = e.target.textContent;
+  console.log(country);
+  searchInput.value = country;
+  searchWrapper.classList.remove("show");
+  input.focus();
+});
+
+let searchable = [
+  "Afghanistan",
+  "Albania",
+  "Algeria",
+  "Andorra",
+  "Angola",
+  "Anguilla",
+  "Antigua &amp; Barbuda",
+  "Argentina",
+  "Armenia",
+  "Aruba",
+  "Australia",
+  "Austria",
+  "Azerbaijan",
+  "Bahamas",
+  "Bahrain",
+  "Bangladesh",
+  "Barbados",
+  "Belarus",
+  "Belgium",
+  "Belize",
+  "Benin",
+  "Bermuda",
+  "Bhutan",
+  "Bolivia",
+  "Bosnia and Herzegovina",
+  "Botswana",
+  "Brazil",
+  "British Virgin Islands",
+  "Brunei",
+  "Bulgaria",
+  "Burundi",
+  "Cambodia",
+  "Cameroon",
+  "Canada",
+  "Cape Verde",
+  "Cayman Islands",
+  "Central Arfrican Republic",
+  "Chad",
+  "Chile",
+  "China",
+  "Colombia",
+  "Congo",
+  "Cook Islands",
+  "Costa Rica",
+  "Cote D Ivoire",
+  "Croatia",
+  "Cuba",
+  "Curacao",
+  "Cyprus",
+  "Czech Republic",
+  "Denmark",
+  "Djibouti",
+  "Dominica",
+  "Dominican Republic",
+  "Ecuador",
+  "Egypt",
+  "El Salvador",
+  "Equatorial Guinea",
+  "Eritrea",
+  "Estonia",
+  "Ethiopia",
+  "Falkland Islands",
+  "Faroe Islands",
+  "Fiji",
+  "Finland",
+  "France",
+  "French Polynesia",
+  "French West Indies",
+  "Gabon",
+  "Gambia",
+  "Georgia",
+  "Germany",
+  "Ghana",
+  "Gibraltar",
+  "Greece",
+  "Greenland",
+  "Grenada",
+  "Guam",
+  "Guatemala",
+  "Guernsey",
+  "Guinea",
+  "Guinea Bissau",
+  "Guyana",
+  "Haiti",
+  "Honduras",
+  "Hong Kong",
+  "Hungary",
+  "Iceland",
+  "India",
+  "Indonesia",
+  "Iran",
+  "Iraq",
+  "Ireland",
+  "Isle of Man",
+  "Israel",
+  "Italy",
+  "Jamaica",
+  "Japan",
+  "Jersey",
+  "Jordan",
+  "Kazakhstan",
+  "Kenya",
+  "Kiribati",
+  "Kosovo",
+  "Kuwait",
+  "Kyrgyzstan",
+  "Laos",
+  "Latvia",
+  "Lebanon",
+  "Lesotho",
+  "Liberia",
+  "Libya",
+  "Liechtenstein",
+  "Lithuania",
+  "Luxembourg",
+  "Macau",
+  "Macedonia",
+  "Madagascar",
+  "Malawi",
+  "Malaysia",
+  "Maldives",
+  "Mali",
+  "Malta",
+  "Marshall Islands",
+  "Mauritania",
+  "Mauritius",
+  "Mexico",
+  "Micronesia",
+  "Moldova",
+  "Monaco",
+  "Mongolia",
+  "Montenegro",
+  "Montserrat",
+  "Morocco",
+  "Mozambique",
+  "Myanmar",
+  "Namibia",
+  "Nepal",
+  "Netherlands",
+  "Netherlands Antilles",
+  "New Caledonia",
+  "New Zealand",
+  "Nicaragua",
+  "Niger",
+  "Nigeria",
+  "North Korea",
+  "Norway",
+  "Oman",
+  "Pakistan",
+  "Palau",
+  "Palestine",
+  "Panama",
+  "Papua New Guinea",
+  "Paraguay",
+  "Peru",
+  "Philippines",
+  "Poland",
+  "Portugal",
+  "Puerto Rico",
+  "Qatar",
+  "Reunion",
+  "Romania",
+  "Russia",
+  "Rwanda",
+  "Saint Pierre &amp; Miquelon",
+  "Samoa",
+  "San Marino",
+  "Sao Tome and Principe",
+  "Saudi Arabia",
+  "Senegal",
+  "Serbia",
+  "Seychelles",
+  "Sierra Leone",
+  "Singapore",
+  "Slovakia",
+  "Slovenia",
+  "Solomon Islands",
+  "Somalia",
+  "South Africa",
+  "South Korea",
+  "South Sudan",
+  "Spain",
+  "Sri Lanka",
+  "St Lucia",
+  "St Vincent",
+  "Sudan",
+  "Suriname",
+  "Swaziland",
+  "Sweden",
+  "Switzerland",
+  "Syria",
+  "Taiwan",
+  "Tajikistan",
+  "Tanzania",
+  "Thailand",
+  "Togo",
+  "Tonga",
+  "Trinidad &amp; Tobago",
+  "Tunisia",
+  "Turkey",
+  "Turkmenistan",
+  "Tuvalu",
+  "Uganda",
+  "Ukraine",
+  "United Arab Emirates",
+  "United Kingdom",
+  "United States of America",
+  "Uruguay",
+  "Uzbekistan",
+  "Vanuatu",
+  "Vatican City",
+  "Venezuela",
+  "Vietnam",
+  "Virgin Islands (US)",
+  "Yemen",
+  "Zambia",
+  "Zimbabwe",
+];
